03d6c682a8656daff5cac174ba27ce7b7f63cd26
jaiboy825 <jaiboy@naver.com>
2021-03-04 10:00:06 +0900

    안드로이드 첫 수업

---
+++ and_workspace/sm3_4/app/src/main/java/com/example/sm3_4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sm3_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1;
+        Button btn2;
+
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2.setText("OKOK");
+            }
+        });
+    }
+}
